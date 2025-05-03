c7f4c898ea1bba500b23c4aa5b5d4dd88cb98fa7
Krzysztof <107839551+Korbinek@users.noreply.github.com>
2025-05-03 23:28:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./modern-normalize.css";
-import "./style.css";
-import "./utils.css";
-import "./components/header.css";
-import "./components/about.css";
-import "./components/hero.css";
+import "/modern-normalize.css";
+import "/style.css";
+import "/utils.css";
+import "/components/header.css";
+import "/components/about.css";
+import "/components/hero.css";
